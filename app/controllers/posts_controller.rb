f695e721be3c06be9ce3de6b5c3be934ccaf443a
Leffler <paine.leffler@rtcmac0etgy6n.int.asurion.com>
2017-07-26 13:00:30 -0500

    full crud on posts with styling

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,44 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(post_params)
+    if @post = Post.create(post_params)
+      flash[:success] = "Your post has been created!"
+      redirect_to posts_path
+    else
+      flash.now[:alert] = "Your new post couldn't be created! Check the form!"
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      flash[:success] = "Post updated."
+      redirect_to(post_path(@post))
+    else
+      flash.now[:alert] = "Update failed. Check the form!"
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
     redirect_to posts_path
   end
 
   private
+
   def post_params
     params.require(:post).permit(:image, :caption)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
